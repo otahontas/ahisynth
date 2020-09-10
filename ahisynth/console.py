a173396ca84909716d3cc917c064047baf37ef4c
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-09-10 22:20:22 +0300

    Initialize midi loading and main entry file

---
+++ ahisynth/console.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+from typing import *
+
+import asyncio
+
+import click
+
+import uvloop
+
+from .midi import get_ports
+
+
+async def async_main() -> None:
+    try:
+        from_circuit, to_circuit = get_ports("Circuit", clock_source=True)
+        from_mono_station, to_mono_station = get_ports("Circuit Mono Station")
+    except ValueError as port:
+        click.secho(f"(port) is not available", fg="red", err=True)
+        raise click.Abort
+
+
+@click.command()
+def main() -> None:
+    uvloop.install()
+    asyncio.run(async_main())
